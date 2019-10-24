7c41d46ae28e887907def7febdfbbb71fa848c4c
AndyYang <40393516+Andyyjg@users.noreply.github.com>
2019-10-24 18:03:25 +0800

    测试11111

---
+++ pdfer-maker/src/lib/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="pdf"  >
+       this is pdf
+
+    </div>
+</template>
+<script>
+
+
+
+    export default {
+
+        name: 'Pdf',
+        components:{
+
+        },
+        data(){
+            return {
+
+            }
+        },
+        mounted: function() {
+        },
+        methods: {}
+
+    }
+</script>
+<style scoped>
+
+</style>
