678ec4b4ac4221942b8e4f986b61b8da7a3fca29
AndyYang <40393516+Andyyjg@users.noreply.github.com>
2023-06-12 15:38:02 +0800

    Update App.vue

--- pdfer-maker/src/App.vue
+++ pdfer-maker/src/App.vue
@@ -12,7 +12,7 @@ export default {
   name: 'app',
   data(){
     return{
-      url:'https://ycism-crm-bj001.oss-cn-beijing.aliyuncs.com/crm/confirm/2019-10-22/Pdf201910221620529501.pdf'
+      url:'https://xxxxxxxxxxxxxxxxxx.pdf'
     }
   },
   components: {
